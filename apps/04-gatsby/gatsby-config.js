ef594d7857f327780786e5788547c27588976d52
nicu <nicu@enovatika.com>
2023-06-25 16:57:18 +0300

    Gatsby: add bundler analyzer

--- apps/04-gatsby/gatsby-config.js
+++ apps/04-gatsby/gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-netlify',
+    'gatsby-plugin-webpack-bundle-analyser-v2',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
